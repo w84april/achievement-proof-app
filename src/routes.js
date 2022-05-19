71bb9fdadd547ccc126f6f257bc04141777d70ba
w84april <donyaschefer@gmail.com>
2022-05-19 15:58:33 +0300

    Init

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+// import
+import Dashboard from "views/Dashboard/Dashboard";
+import Tables from "views/Dashboard/Tables";
+import Billing from "views/Dashboard/Billing";
+import RTLPage from "views/Dashboard/RTL";
+import Profile from "views/Dashboard/Profile";
+import SignIn from "views/Auth/SignIn.js";
+import SignUp from "views/Auth/SignUp.js";
+import { AddIcon } from "@chakra-ui/icons";
+
+import {
+  HomeIcon,
+  StatsIcon,
+  CreditIcon,
+  PersonIcon,
+  DocumentIcon,
+  RocketIcon,
+  SupportIcon,
+} from "components/Icons/Icons";
+
+var dashRoutes = [
+  {
+    path: "/dashboard",
+    name: "Проекты",
+    icon: <HomeIcon color="inherit" />,
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Создать заявку",
+    rtlName: "لوحة القيادة",
+    icon: <AddIcon color="inherit" w={3} h={3} />,
+    component: Tables,
+    layout: "/admin",
+  },
+  {
+    path: "/billing",
+    name: "Награды",
+
+    icon: <CreditIcon color="inherit" />,
+    component: Billing,
+    layout: "/admin",
+  },
+
+  {
+    name: "Аккаунт",
+    category: "account",
+
+    state: "pageCollapse",
+    views: [
+      {
+        path: "/profile",
+        name: "Профиль",
+        icon: <PersonIcon color="inherit" />,
+        secondaryNavbar: true,
+        component: Profile,
+        layout: "/admin",
+      },
+    ],
+  },
+];
+export default dashRoutes;
